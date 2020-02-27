ba249c6f3e4061f619721ead4e7802210c779e4e
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-02-27 10:57:56 +0100

    Updates

---
+++ precache-manifest.a64245f24174f82ff72ca869a06e15f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d505803de0d3a42e6476089ab8ba4403",
+    "url": "/movie-redux/index.html"
+  },
+  {
+    "revision": "214de5b9f5fe1eb442ad",
+    "url": "/movie-redux/static/css/main.706f075a.chunk.css"
+  },
+  {
+    "revision": "7be29781c6db390c0f9b",
+    "url": "/movie-redux/static/js/2.da5e33b3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-redux/static/js/2.da5e33b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214de5b9f5fe1eb442ad",
+    "url": "/movie-redux/static/js/main.cbbbcd7f.chunk.js"
+  },
+  {
+    "revision": "4eb29871dd86f5d07619",
+    "url": "/movie-redux/static/js/runtime-main.cb6e2147.js"
+  }
+]);
